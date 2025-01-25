55a94b5180e4bbef22f09639e696f4c850f8b929
Hadrien <ketsapiwiq@protonmail.com>
2025-01-25 12:27:00 +0100

    add migrations

---
+++ Jellyfin.Server.Implementations/Migrations/20250125112553_AddShowOriginalTitleToDisplayPreferences.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Jellyfin.Server.Implementations.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddShowOriginalTitleToDisplayPreferences : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "ShowOriginalTitle",
+                table: "DisplayPreferences",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ShowOriginalTitle",
+                table: "DisplayPreferences");
+        }
+    }
+}
